bef622e113bd77abea54bbc7568fd9ed52d81843
Daniel Warner <danywarner@gmail.com>
2016-03-02 13:47:19 -0500

    sections navigation through JS

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+function addListeners() {
+
+	document.querySelector("#home").addEventListener("click", toggleContent,false);
+	document.querySelector("#propuesta").addEventListener("click", toggleContent,false);
+	document.querySelector("#memoria").addEventListener("click", toggleContent,false);
+	document.querySelector("#entregables").addEventListener("click", toggleContent,false);
+
+}
+
+function toggleContent(e) {
+	if (!e)
+        e = window.event;
+    var sender = e.srcElement || e.target;
+
+    while (sender && sender.nodeName.toLowerCase() != "li")
+        sender = sender.parentNode;
+
+     var myId = sender.id;
+     myId+="Content";
+     
+     var showings = document.querySelectorAll(".show");
+     for (var x = 0 ; x < showings.length ; x++) {
+     	var showing = showings[x];
+     	showing.classList.remove("show");
+     	showing.classList.add("hide");
+     }
+     var toShow = document.getElementById(myId);
+     
+     toShow.classList.remove("hide");
+	 toShow.classList.add("show");
+     
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  addListeners();
+});
